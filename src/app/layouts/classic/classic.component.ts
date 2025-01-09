1697bbfdcfc675cf4a9c88932eabe13bc0598b17
salloumshamel <shamel.salloum.offical@gmail.com>
2025-01-09 10:57:26 +0300

    add theming button and enhance the landing sections

--- src/app/layouts/classic/classic.component.ts
+++ src/app/layouts/classic/classic.component.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {ThemeToggleComponent} from '../components/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-classic',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    ThemeToggleComponent
   ],
   templateUrl: './classic.component.html',
   styleUrl: './classic.component.scss'
---
+++ src/app/layouts/components/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-theme-toggle',
+  imports: [],
+  templateUrl: './theme-toggle.component.html',
+  styleUrl: './theme-toggle.component.scss'
+})
+export class ThemeToggleComponent {
+}
